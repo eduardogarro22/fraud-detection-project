b6de781e6bfa9819a6caf2f4f27f7477d00e02d8
eduardogarro22 <eduardogarro23@gmail.com>
2026-03-09 00:05:34 -0300

    ETL pipeline funcionando: carga de transacciones en SQL Server

--- src/etl/etl_transacciones.py
+++ src/etl/etl_transacciones.py
@@ -1,29 +1,30 @@
 import pandas as pd
-from db_connection import engine
+from src.database.db_connection import get_engine
 from sqlalchemy import text
 from datetime import datetime, timedelta
 
 # 1. Leer datos crudos (limitamos para prueba)
 query = """
 SELECT TOP 100
-    Time,
-    Amount,
-    Class
+    time_seconds,
+    amount,
+    is_fraud
 FROM Fraud_Transactions_Raw
-ORDER BY Time
+ORDER BY time_seconds
 """
+engine = get_engine()
 
 df = pd.read_sql(query, engine)
 
 # 2. Transformaciones
 start_date = datetime(2024, 1, 1)
 
-df["fecha"] = df["Time"].apply(
+df["fecha"] = df["time_seconds"].apply(
     lambda x: start_date + timedelta(seconds=float(x))
 )
 
-df["monto"] = df["Amount"]
-df["es_fraude"] = df["Class"]
+df["monto"] = df["amount"]
+df["es_fraude"] = df["is_fraud"]
 
 df["cuenta_id"] = 1  # simulamos una cuenta
 df["tipo_transaccion"] = "compra"
